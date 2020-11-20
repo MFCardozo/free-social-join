665ae5ffebddbbf63d9968546875495130c24207
ManuMcfly6 <manu.mcfly6@gmail.com>
2020-11-20 16:15:52 -0300

    fix

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -15,7 +15,7 @@ class PostsController extends Controller
 
     public function index(){
 
-        //plucck find the specific key in array ,that you passed a return the values
+        //pluck find the specific key in array ,that you passed a return the values
 
         $users=auth()->user()->following()->pluck('profiles.user_id');
         
