3894b97b040cd4d709dafee7a862a1dfa510bba3
ManuMcfly6 <manu.mcfly6@gmail.com>
2020-11-20 20:44:43 -0300

    fix bugs

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,13 +10,13 @@
     <div class="row py-4 px-2">
         <div class="col-sm-12 col-md-6  offset-md-3  card">
             <a href="../p/{{$post->id}}">
-            <img src="https://freesocialjoin.s3.us-east-2.amazonaws.com/{{ $post->image }}" class="mw-100 rounded pt-1" alt={{$post->caption}}>
+            <img src="https://freesocialjoin.s3.us-east-2.amazonaws.com/{{ $post->image }}" class="mw-100 rounded pt-1" alt="{{$post->caption}}">
                 </a>
              
                 
                  <div class="d-flex align-items-baseline py-2">
                      <div class="pr-3">
-                        <img src="{{$post->user->profile->profileImage()}}" class="rounded-circle w-100 " style="max-width: 40px;">
+                        <img src="{{$post->user->profile->profileImage()}}" class="rounded-circle w-100" style="max-width: 40px;">
                     </div>
                     <span>
                         <a href="/profile/{{ $post->user->id }}">
