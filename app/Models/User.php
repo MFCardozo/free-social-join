d3f10984fcfa0b1cce8b42bbadcea0bf721e7a7a
ManuMcfly6 <manu.mcfly6@gmail.com>
2020-11-20 20:06:20 -0300

    fix Images

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    /**
+    /***
      * this function act as an dispatch whenever a user is created
      * we created a profile to same user
      */
